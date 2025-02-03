8ff71502974a51c5a1ccfb1933482a5ae4818a11
House409 <peeraput.jaikla@gmail.com>
2025-02-03 14:51:39 +0700

    ✨ feat: add entities for graph all disease

--- features/graph_disease/entities/graph_disease.go
+++ features/graph_disease/entities/graph_disease.go
@@ -9,40 +9,34 @@ type DiseaseRequest struct {
 }
 
 type DiseaseResponse struct {
-	Status  bool     `json:"status"`
-	Message string   `json:"message"`
-	Result  []CaData `json:"result"`
+	Status  bool          `json:"status"`
+	Message string        `json:"message"`
+	Result  []DiseaseData `json:"result"`
 }
 
-type CidCountData struct {
-	Year     int     `bson:"year" json:"year"`
-	Month    int     `bson:"month" json:"month"`
-	CidCount float64 `bson:"cid_count" json:"cid_count"`
-}
-
-type CaData struct {
+type DiseaseData struct {
 	DiseaseName string    `json:"diseaseName"`
 	Data        []float64 `json:"data"`
 }
 
-type CaExpenseData struct {
-	Year                int     `bson:"year" json:"year"`
-	Month               int     `bson:"month" json:"month"`
-	CaExpense           float64 `bson:"ca_expense" json:"ca_expense"`
-	LungCaExpense       float64 `bson:"lung_ca_expense" json:"lung_ca_expense"`
-	BreastCaExpense     float64 `bson:"breast_ca_expense" json:"breast_ca_expense"`
-	CervicalCaExpense   float64 `bson:"cervical_ca_expense" json:"cervical_ca_expense"`
-	LiverCaExpense      float64 `bson:"liver_ca_expense" json:"liver_ca_expense"`
-	ColorectalCaExpense float64 `bson:"colorectal_ca_expense" json:"colorectal_ca_expense"`
+type DiseaseExpenseData struct {
+	Year         int     `bson:"year" json:"year"`
+	Month        int     `bson:"month" json:"month"`
+	DMExpense    float64 `bson:"dm_expense" json:"dm_expense"`
+	HtExpense    float64 `bson:"ht_expense" json:"ht_expense"`
+	CopdExpense  float64 `bson:"copd_expense" json:"copd_expense"`
+	CaExpense    float64 `bson:"ca_expense" json:"ca_expense"`
+	PsyExpense   float64 `bson:"psy_expense" json:"psy_expense"`
+	HdCvdExpense float64 `bson:"hd_cvd_expense" json:"hd_cvd_expense"`
 }
 
-type CaPatientData struct {
-	Year                 int     `bson:"year" json:"year"`
-	Month                int     `bson:"month" json:"month"`
-	CaCidCount           float64 `bson:"ca_cid_count" json:"ca_cid_count"`
-	LungCaCidCount       float64 `bson:"lung_ca_cid_count" json:"lung_ca_cid_count"`
-	BreastCaCidCount     float64 `bson:"breast_ca_cid_count" json:"breast_ca_cid_count"`
-	CervicalCaCidCount   float64 `bson:"cervical_ca_cid_count" json:"cervical_ca_cid_count"`
-	LiverCaCidCount      float64 `bson:"liver_ca_cid_count" json:"liver_ca_cid_count"`
-	ColorectalCaCidCount float64 `bson:"colorectal_ca_cid_count" json:"colorectal_ca_cid_count"`
+type DiseasePatientData struct {
+	Year          int     `bson:"year" json:"year"`
+	Month         int     `bson:"month" json:"month"`
+	DMCidCount    float64 `bson:"dm_cid_count" json:"dm_cid_count"`
+	HtCidCount    float64 `bson:"ht_cid_count" json:"ht_cid_count"`
+	CopdCidCount  float64 `bson:"copd_cid_count" json:"copd_cid_count"`
+	CaCidCount    float64 `bson:"ca_cid_count" json:"ca_cid_count"`
+	PsyCidCount   float64 `bson:"psy_cid_count" json:"psy_cid_count"`
+	HdCvdCidCount float64 `bson:"hd_cvd_cid_count" json:"hd_cvd_cid_count"`
 }

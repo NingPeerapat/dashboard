82655b208d67e2a41b07262c5504b3928f735886
House409 <peeraput.jaikla@gmail.com>
2025-02-06 17:10:14 +0700

    🦄 refactor: change method to connect collection and add collection temp data

--- pkg/database/mongo.go
+++ pkg/database/mongo.go
@@ -12,9 +12,10 @@ import (
 )
 
 type Config struct {
-	MongoURI      string
-	DatabaseName  string
-	CollectionName string
+	MongoURI           string
+	DatabaseName       string
+	CollectionName     string
+	CollectionTempName string
 }
 
 func LoadConfig() (*Config, error) {
@@ -25,27 +26,31 @@ func LoadConfig() (*Config, error) {
 	}
 
 	return &Config{
-		MongoURI:     os.Getenv("MONGO_URI"),
-		DatabaseName: os.Getenv("DATABASE_NAME"),
-		CollectionName: os.Getenv("COLLECTION_NAME"),
+		MongoURI:           os.Getenv("MONGO_URI"),
+		DatabaseName:       os.Getenv("DATABASE_NAME"),
+		CollectionName:     os.Getenv("COLLECTION_NAME"),
+		CollectionTempName: os.Getenv("COLLECTION_TEMP_NAME"),
 	}, nil
 }
 
 // เชื่อมต่อ MongoDB ด้วยการใช้ URI จาก config
-func ConnectMongo(cfg *Config) (*mongo.Client, error) {
+func ConnectMongo(cfg *Config) (*mongo.Collection, *mongo.Collection, error) {
 	clientOptions := options.Client().ApplyURI(cfg.MongoURI)
 
 	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
-		return nil, fmt.Errorf("error connecting to MongoDB: %v", err)
+		return nil, nil, fmt.Errorf("error connecting to MongoDB: %v", err)
 	}
 
 	// ตรวจสอบว่าเชื่อมต่อได้หรือไม่
 	err = client.Ping(context.Background(), nil)
 	if err != nil {
-		return nil, fmt.Errorf("error pinging MongoDB: %v", err)
+		return nil, nil, fmt.Errorf("error pinging MongoDB: %v", err)
 	}
 
+	collection := client.Database(cfg.DatabaseName).Collection(cfg.CollectionName)
+	collectionTemp := client.Database(cfg.DatabaseName).Collection(cfg.CollectionTempName)
+
 	log.Println("Connected to MongoDB")
-	return client, nil
+	return collection, collectionTemp, nil
 }
